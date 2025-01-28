72c2d2e61c30acdb43d3a184a94080659689ef8f
Bkotay <bkotay4@gmail.com>
2025-01-28 17:23:54 -0500

    changes

--- v2/src/pages/Home.jsx
+++ v2/src/pages/Home.jsx
@@ -8,7 +8,6 @@ import Footer from '../components/Footer';
 
 const Home = () => (
   <div>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <Header />
     <Hero />
     <AboutUs />
--- v2/src/pages/Product.jsx
+++ v2/src/pages/Product.jsx
@@ -8,7 +8,6 @@ import ProductTop from '../components/ProductTop';
 
 const Product = () => (
   <div>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     <Header />
    <ProductTop />
     <ProductFilter />
